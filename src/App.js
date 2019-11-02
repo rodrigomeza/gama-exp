b1e2869556c708c207690aff9333c5eb55b03c14
Rodrigo Meza <jremeza@gmail.com>
2019-11-02 11:06:49 -0300

    versao 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quem é o James?</h1>
+      <h5>O que ele come? </h5>
+      <p>Onde ele anda?</p>
+    </div>
+  );
+}
+
+export default App;
